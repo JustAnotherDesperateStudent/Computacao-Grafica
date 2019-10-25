b52b3a713a296c65ce71b74c4c46f07f52bf0316
MadeYouRead-This <56697153+MadeYouRead-This@users.noreply.github.com>
2019-10-25 09:10:08 -0300

    Update lunarmodule.py

--- lunarmodule.py
+++ lunarmodule.py
@@ -2,7 +2,7 @@ from __future__ import division
 from vpython import *
 
 
-# build the scene
+# Construindo o cenário
 scene.width = 600
 scene.height = 400
 scene.range = 75
@@ -17,22 +17,22 @@ print("Press left and right arrow keys to rotate.\nHold up arrow to apply thrust
 N_stars = 200
 star = [] 
 for i in range(N_stars):
-  # don't get too caught up in the numbers below 
-  # I was just trying to make a nice, randomized starfield
-  # and just played with a bunch of values to make it look good
+  # Numeros aleatorios
+  # Criando um campo de estrelas
+  # varios valores foram testados para tentar parecer mais bonito
   star.append(sphere(radius = 0.25 + 0.75 * random()))
   star[i].color = vector(0.85, 0.85, 0.85) + 0.15 * vector(random(), 0, random())
   star[i].pos.z = -250 + 100 * random()
   star[i].pos.x = 6 * scene.range * (random() - 0.5)
   star[i].pos.y = 20 + 6 * scene.range * (random() - 0.5)
 
-# build the landing pads and the truncated-cone mountains beneath them
-pad = []   # cylinder pad
-L = []   # height(s) of mountain (length of truncated cone)
-R1 = []  # small radii of truncated cones 
-R2 = []  # large radii of truncated cones 
-s = []   # shapes to be extruded to form cone
-c = []   # cones (i.e., mountains) 
+# Criando as zonas de pouso e as montanhas "cortadas" embaixo
+pad = []   # cilindro
+L = []   # tamanho(s) da(s) montanha(s) (tamanho do cone cortado)
+R1 = []  # pequenos raios dos cones cortados 
+R2 = []  # grandes raio dos cones cortados
+s = []   # formas para formarem o cone cortado
+c = []   # cones 
 
 N_pads = 15
 
@@ -42,7 +42,7 @@ for i in range(N_pads):
   pad[i].pos.z = 0
   pad[i].pos.y = -50 + 50 * random()
   pad[i].pos.x = -scene.range + i * ((1 + 2 * random())*scene.range/N_pads)  
-  # make a mountain (thanks Bruce Sherwood!)
+  # Fazendo a montanha
   L.append(50 - (pad[i].pos.y))
   R1.append(15 + 100*random())
   R2.append(pad[i].radius)
@@ -52,7 +52,7 @@ for i in range(N_pads):
   c[i].texture = textures.rough
   c[i].bumpmap = bumpmaps.stucco
 
-# build the spacecraft
+# Construindo o módulo lunar
 
 capsule = cylinder(axis = vector(0,3,0), radius = 2.0)
 # Parametros do VPython
@@ -80,16 +80,16 @@ flame.texture = textures.wood_old
 
 def process(event):
   if running == False: return 
-  if (event.which == 37):   # left
+  if (event.which == 37):   # esquerda
     capsule.rotate(axis = vector(0,0,1), angle = capsule.rotangle)
     legs.rotate(axis = vector(0,0,1), angle = capsule.rotangle)
     capsule.angle = capsule.angle + capsule.rotangle
-  if (event.which == 39):   # right
+  if (event.which == 39):   # direita
     capsule.rotate(axis = vector(0,0,1), angle = -capsule.rotangle)
     legs.rotate(axis = vector(0,0,1), angle = -capsule.rotangle)
     capsule.angle = capsule.angle - capsule.rotangle
-  if (event.which == 38):   # up
-    # length of flame is 1 m per 1000 thrust
+  if (event.which == 38):   # cima
+    # Tamanho da chama é 1 m por 1000 "impulsos"(thrust)
     capsule.thrust = capsule.thrust + capsule.thrustincrement
     if (capsule.fuel < 0):
       capsule.thrust = 0.0
@@ -102,94 +102,96 @@ def letgo(event):
     capsule.thrust = 0
 
 
-#keyboard processing 
+# Processando as ações do teclado
 scene.bind('click keydown', process)
 scene.bind('click keyup', letgo)
 
-# data label for top-right corner
+# Area de dados para o canto direito superior
 data = label(box = False, text = '', visible = True, height = 11, pixel_pos = True, align = 'left')
 data.pos = vector(scene.width - 150,scene.height - 20,0)
 
-# debug label for top-left corner
+# Area de debug para o canto esquero superior
 debug = label(box = False, text = '', visible = False, height = 11, pixel_pos = True, align = 'left')
 debug.pos = vector(50 ,scene.height - 50,0)
 
-# announcement label
+# Area de anuncios
 announce = label(box = False, text = '', visible = False, height = 30, pixel_pos = True, align ='center', opacity = 0.0 )
 announce.pos = vector(scene.width/2, 125 + scene.height/2, 0)
 
-# physics
-g = 0.8   # N/kg  (note: moon should be 1.6)
+# Fisica
+g = 0.8   # N/kg
 dt = 0.001
 running = True 
 
-# animation and main gameplay loop 
+# Loop da animação
 while(running):
 
   rate(1/dt)
   
-  # determine forces on capsule 
+  # Forcas do modulo
   force = vector(0, -capsule.mass * g, 0) 
   force = force + capsule.axis.norm() * capsule.thrust 
   
-  # remove fuel
+  # Removendo combustivel
   capsule.fuel = capsule.fuel - capsule.thrust * dt / 1000
   if (capsule.fuel < 20):
     announce.color = color.red
     announce.visible = True 
     announce.text = "Low on fuel!"
   
-  # update capsule kinematics 
+  # Atualizar o estado do modulo 
   capsule.vel = capsule.vel + (force / capsule.mass) * dt
   capsule.pos = capsule.pos + capsule.vel * dt
   flame.pos = capsule.pos + 0.25 * capsule.axis
   legs.pos = capsule.pos + 0.25 * capsule.axis
   
-  # flame "flutter" - just a cool effect
+  # Tentativa de adicionar efeito ao fogo
   flame.axis = - capsule.axis.norm() * (25 * capsule.thrust / capsule.maxthrust) 
   #flame.axis = flame.axis + 0.3 * random() * flame.axis + 0.02 * flame.axis.mag * vectortor.random()
   flame.opacity = 0.5 + 0.25*random()
   flame.color = vector(0.5 + 0.5 * random(), 0.35*random(), 0.35*random())
     
-  # check for mountains and contact with pads
-  zoom = False   # zoom in if near a pad or mountain wall 
+  # Checando por montanhas e contato com areas de pouso
+  zoom = False   # Zoom in se perto de uma montanha ou zona de pouso 
   for i in range(len(pad)):
 
-    # check to see if we are near a mountain wall - treat each as a line slope
-    # left walls
-    m = L[i] / (R1[i] - R2[i])   # slope of wall 
-    b = pad[i].pos.y - m * (pad[i].pos.x - pad[i].radius)   # offset of wall
-    if ((capsule.pos.y < (m * capsule.pos.x + b)) # if we are underneath the slope
-      and (capsule.pos.x < pad[i].pos.x - pad[i].radius) # and we are left of the pad
-      and (capsule.pos.y - (pad[i].pos.y + pad[i].axis.y) < 0.0)):  # and we are under the pad
-      running = False 
-      flame.axis = vector(0,0,0)
-      announce.color = color.red
-      announce.visible = True 
-      announce.text = "Crash landing!"
-   # right walls
+    # Checar para ver se esta perto a uma montanha - tratar todas como uma linha inclinada
+    
+    # Paredes à esquerda
+    m = L[i] / (R1[i] - R2[i])   # Declive da parede 
+    b = pad[i].pos.y - m * (pad[i].pos.x - pad[i].radius)   # Deslocamento da parede
+    if ((capsule.pos.y < (m * capsule.pos.x + b)) # Se estiver embaixo da parede
+      and (capsule.pos.x < pad[i].pos.x - pad[i].radius) # Se estiver a esquerda da zona de pouso
+      and (capsule.pos.y - (pad[i].pos.y + pad[i].axis.y) < 0.0)):  # Se estiver embaixo da zona de pouso
+        running = False 
+        flame.axis = vector(0,0,0)
+        announce.color = color.red
+        announce.visible = True 
+        announce.text = "Crash landing!"
+    
+    # Paredes à direita
     m = - m
-    b = pad[i].pos.y - m * (pad[i].pos.x + pad[i].radius)   # offset of wall
-    if ((capsule.pos.y < (m * capsule.pos.x + b)) # if we are underneath the slope
-      and (capsule.pos.x > pad[i].pos.x - pad[i].radius)  # and we are right of the pad
-      and (capsule.pos.y - (pad[i].pos.y + pad[i].axis.y) < 0.0)):   # and we are under the pad
-      running = False 
-      flame.axis = vector(0,0,0)
-      announce.color = color.red
-      announce.visible = True 
-      announce.text = "Crash landing!"
+    b = pad[i].pos.y - m * (pad[i].pos.x + pad[i].radius)   # Deslocamento da parede
+    if ((capsule.pos.y < (m * capsule.pos.x + b)) # Se estiver embaixo da parede
+      and (capsule.pos.x > pad[i].pos.x - pad[i].radius)  # Se estiver a esquerda da zona de pouso
+      and (capsule.pos.y - (pad[i].pos.y + pad[i].axis.y) < 0.0)):   # Se estiver embaixo da zona de pouso
+        running = False 
+        flame.axis = vector(0,0,0)
+        announce.color = color.red
+        announce.visible = True 
+        announce.text = "Crash landing!"
  
-    # check to see if we are near a landing pad
-    horsep = abs(capsule.pos.x - pad[i].pos.x)  # horizontal separation between capsule and pad
-    versep = capsule.pos.y - 1 - (pad[i].pos.y + pad[i].axis.y)   # vertical separation between capsule and pad top
-    # should we zoom in? 
+    # Checar se está perto de uma zona de pouso
+    horsep = abs(capsule.pos.x - pad[i].pos.x)  # Separação horizontal entre o módulo e a zona
+    versep = capsule.pos.y - 1 - (pad[i].pos.y + pad[i].axis.y)   # Separação vertical entre o módulo e a zona
+    # Devemos aumentar o zoom?
     if (horsep < 3 * pad[i].radius and versep < 4 * pad[i].radius and versep > 0):
       zoom = True
       scene.center = pad[i].pos + vector(0, pad[i].radius, 0)
       offset = pad[i].pos.y + pad[i].axis.y
       scene.range = 3 * pad[i].radius 
     
-    # are we attempting a landing?  
+    # Estamos tentando pousar?
     if ((horsep < 0.85 * pad[i].radius) and (abs(versep) < 0.1 * capsule.axis.mag)): 
       running = False 
       flame.axis = vector(0,0,0)
@@ -208,13 +210,13 @@ while(running):
         if (abs(capsule.vel.y) >= 1.5): 
           announce.text += "\nVertical speed too high!"
   
-  # reset zoom if we didn't zoom in on any pads
+  # Resetar o zoom se não tivermos dado zoom in em nenhuma zona de pouso
   if (zoom == False):
     scene.range = 75
     scene.center = vector(0, 10, 0)
     offset = -50
     
-  # update data labels
+  # Atualizando a área de dados
   data.text = 'Altitude: ' + str((round(10*(capsule.pos.y - 1 - offset))/10)) + " m"
   data.text += '\nHorizontal velocity: ' + str(round(10*capsule.vel.x)/10) + " m/s"
   data.text += '\nVertical velocity: ' + str(round(10*capsule.vel.y)/10) + " m/s"
